d39560776cc6dd90fce0f2ad38b00b3d651a620a
lmtuan <lmtuan98@gmail.com>
2017-04-21 15:02:44 -0400

    Add fillHitbox function

--- Board.cpp
+++ Board.cpp
@@ -2,54 +2,87 @@
 #include <PieceType.h>
 #include <stdio.h>
 
+
 Board::Board()
 {
-  player = 1;
-  chosenSquare = -1;
   initBoard();
 }
 Board::~Board() {};
 
+const int Board::dir[8][2] = { {0, 1}, {0, -1}, {1, 0}, {-1, 0},
+                               {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+const int Board::dirKnight[8][2] = { {1, 2}, {1, -2}, {-1, 2}, {-1, -2},
+                                     {2, 1}, {2, -1}, {-2, 1}, {-2, -1} };
 void Board::initBoard(){
-  int i,j;
-  for (j=0;j<8;j++){
-      board[1][j] = WP;
-      board[6][j] = BP;
-  }
-  for (i = 2; i < 6; i++)
+  player = 1;
+  chosenSquare = -1;
+
+  /*
+   * board after init
+   *
+   * 25 26 27 28 29 30 31 32
+   * 17 18 19 20 21 22 23 24
+   *  0  0  0  0  0  0  0  0
+   * ...
+   * ...
+   *  9 10 11 12 13 14 15 16
+   *  1  2  3  4  5  6  7  8
+   *
+   * piece after init
+   * {-1, WR, WN, WB, WQ ... BN, BR}
+   */
+
+  // Fill board
+  // Fill all square with ID = 0 empty
+  for(int i = 0; i < 64; i++)
   {
-    for (j = 0; j < 8; j++)
-    {
-      board[i][j] = -1;
-    }
+    board[i/8][i%8] = 0;
+  }
+  for(int i = 0; i < 16; i++)
+  {
+    board[i/8][i%8] = i+1;
+  }
+  for(int i = 16; i < 32; i++)
+  {
+    board[4 + i/8][i%8] = i+1;
   }
 
-  board[0][0]= WR;
-  board[0][1]= WN;
-  board[0][2]= WB;
-  board[0][3]= WQ;
-  board[0][4]= WK;
-  board[0][5]= WB;
-  board[0][6]= WN;
-  board[0][7]= WR;
-
-  board[7][0]= BR;
-  board[7][1]= BN;
-  board[7][2]= BB;
-  board[7][3]= BQ;
-  board[7][4]= BK;
-  board[7][5]= BB;
-  board[7][6]= BN;
-  board[7][7]= BR;
+  //Fill pieces
+  pieces[0] = -1;
+  pieces[1] = WR;
+  pieces[2] = WN;
+  pieces[3] = WB;
+  pieces[4] = WQ;
+  pieces[5] = WK;
+  pieces[6] = WB;
+  pieces[7] = WN;
+  pieces[8] = WR;
+  for (int i = 9; i <= 16; i++)
+  {
+    pieces[i] = WP;
+  }
+  for(int i = 17; i <= 24; i++)
+  {
+    pieces[i] = BP;
+  }
+  pieces[25] = BR;
+  pieces[26] = BN;
+  pieces[27] = BB;
+  pieces[28] = BQ;
+  pieces[29] = BK;
+  pieces[30] = BB;
+  pieces[31] = BN;
+  pieces[32] = BR;
 }
 
 int Board::getPiece(int square)
 {
+  int piece = pieces[ board[square/8][square%8] ];
   if (square == chosenSquare)
   {
-    return board[square/8][square%8] + 12;
+    return piece + 12;
   }
-  return board[square/8][square%8];
+  return piece;
 }
 
 void Board::chooseSquare(int square)
@@ -83,8 +116,8 @@ void Board::chooseSquare(int square)
 bool Board::makeMove(int x1,int y1,int x2,int y2)
 {
     if (isMoveLegal(x1, y1, x2, y2)){
-        board[x2][y2]=board[x1][y1];
-        board[x1][y1] = -1;
+        board[x2][y2] = board[x1][y1];
+        board[x1][y1] = 0;
         return true;
     }
     return false;
@@ -95,3 +128,158 @@ bool Board::isMoveLegal(int x1, int y1, int x2, int y2)
     return true;
 }
 
+void Board::fillHitbox(int square)
+{
+  int pID = board[square/8][square%8];
+  // empty piece
+  if(pID == 0) return;
+  int pType = pieces[pID];
+  switch(pType)
+  {
+    //case WP: case BP: fillHitboxPawn(square, pID); break;
+    case WR: case BR:
+    case WB: case BB:
+    case WQ: case BQ: fillHitboxRayPieces(square, pID); break;
+
+  }
+}
+
+void Board::fillHitboxPawn(int square, int pID)
+{
+
+}
+
+void Board::fillHitboxKnight(int square, int pID)
+{
+  int i,j;
+  /*
+   * Fill hitbox[pID] with illegal move
+   */
+  for (i = 0; i < 8; i++)
+  {
+    for(j = 0; j < 8; j++)
+      {
+        hitboxes[pID][i][j] = MOVE_ILLEGAL;
+      }
+  }
+
+  /*
+   * Look in 8 knight direction and add legal move
+   */
+  // Coordinate of the piece
+  int r = square / 8;
+  int c = square % 8;
+  // Look in 8 direction
+  for (int d = 0; d < 8; d++)
+  {
+    i = r + dirKnight[d][0];
+    j = c + dirKnight[d][1];
+    // if out of bound, check other directions
+    if ( i < 0 || i > 7 || j < 0 || j > 7) continue;
+
+    if(board[i][j] == 0) //if square empty, legal move
+    {
+      hitboxes[pID][i][j] = MOVE_NORMAL;
+    }
+    else if ( (board[r][c] <= 16) != (board[i][j] <= 16) ) // if square has opponent's piece, legal move
+    {
+      hitboxes[pID][i][j] = MOVE_NORMAL;
+    }
+  }
+}
+
+void Board::fillHitboxRayPieces(int square, int pID)
+{
+  int i,j;
+  /*
+   * Fill all with illegal move
+   */
+  for (i = 0; i < 8; i++)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      hitboxes[pID][i][j] = MOVE_ILLEGAL;
+    }
+  }
+  int minDir, maxDir;
+  /*
+   * Choose direction
+   */
+  switch(pieces[pID])
+  {
+    case WR: case BR:
+      minDir = 0; maxDir = 3;
+    case WB: case BB:
+      minDir = 4; maxDir = 7;
+    case WQ: case BQ:
+      minDir = 0; maxDir = 7;
+   }
+   /*
+    * Look into all directions
+    * Fill hitbox
+    */
+  // coordinate of piece
+  int r = square/8;
+  int c = square%8;
+  // Look into all directions
+  for(int d = minDir; d <= maxDir; d++)
+  {
+    i = r;
+    j = c;
+    while(true)
+    {
+      // advance 1 square in the current direction
+      i += dir[d][0];
+      j += dir[d][1];
+      if (i < 0 || i > 7 || j < 0 || j > 7) break;
+      if (board[i][j] == 0) // if empty square, potential move
+      {
+        hitboxes[pID][i][j] = MOVE_NORMAL;
+      }
+      else // if meet obstacle
+      {
+        if( (pID <= 16) != (board[i][j] <= 16) ) //if opposite color, still legal
+        {
+          hitboxes[pID][i][j] = MOVE_NORMAL;
+        }
+        i += dir[d][0];
+        j += dir[d][1];
+        hitboxes[pID][i][j] = MOVE_BEHIND_OBSTACLE;
+        break;
+      }
+    }
+  }
+}
+
+void Board::fillHitboxKing(int square, int pID)
+{
+  int i,j;
+  /*
+   * Fill hitbox with illegal moves
+   */
+  for (i = 0; i < 8; i++)
+  {
+    for(j = 0; j < 8; j++)
+      {
+        hitboxes[pID][i][j] = MOVE_ILLEGAL;
+      }
+  }
+
+  /*
+   * Look into 8 directions and find legal move
+   */
+  int r = square/8;
+  int c = square%8;
+
+  for(int d = 0; d < 8; d++)
+  {
+    i = r + dir[d][0];
+    j = c + dir[d][1];
+
+    if(i < 0 || i > 7 || j < 0 || j > 7) continue;
+    if((board[i][j] <= 16) != (board[r][c] <= 16))
+    {
+      hitboxes[pID][i][j] = MOVE_NORMAL;
+    }
+  }
+}
--- Board.h
+++ Board.h
@@ -24,18 +24,32 @@ class Board
      *   .. .. ..
      *   08 .. 15
      *   00 .. 07
-     * @return piece according to enum pieceType, -1 if empty
+     * @return type of piece in square according to enum pieceType, -1 if empty, +12 if square is chosen
+     *
      */
     int getPiece(int square);
 
     /**
-     * Notify the board that a player has clicked a square
+     * Call when a player clicks a square
      * @param square the square number between 0 and 63, -1 if user click outside the board
      */
     void chooseSquare(int square);
 
   private:
+    /**
+     * Index: row and column
+     * Value: ID of piece
+     */
     int board[8][8];
+    /**
+     * Index: ID of piece, index 0 is empty piece
+     * Value: Type of piece
+     */
+    int pieces[33];
+
+    /**
+     * 1 is the first player, 2 is the second player
+     */
     int player;
 
     /**
@@ -43,6 +57,26 @@ class Board
      * -1 if no chosen square, else 0 to 63
      */
     int chosenSquare;
+
+
+
+    int hitboxes[32][8][8];
+    enum moveType {
+      MOVE_ILLEGAL,
+      MOVE_SELF, //the square where the piece is
+      MOVE_NORMAL,
+      MOVE_ENPASSANT,
+      MOVE_CASTLING,
+      MOVE_BEHIND_OBSTACLE //an illegal square of ray piece, directly after an obstacle
+    };
+    /**
+     * 8 basic direction
+     */
+    static const int dir[8][2];
+    /**
+     * 8 knight's direction
+     */
+    static const int dirKnight[8][2];
 
     /**
      * move a piece from square (x1, y1) to (x2, y2)
@@ -55,6 +89,20 @@ class Board
 
 
     bool isMoveLegal(int x1, int y1, int x2, int y2);
+
+    /**
+     * Fill a hitbox with move Types
+     * @param pID: ID of piece
+     */
+    void fillHitbox(int square);
+
+    /**
+     * Fill a hitbox of a specific type of piece with move types
+     */
+    void fillHitboxPawn(int square, int pID);
+    void fillHitboxKnight(int square, int pID);
+    void fillHitboxKing(int square, int pID);
+    void fillHitboxRayPieces(int square, int pID);
 };
 
 #endif // BOARD_H
